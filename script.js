d605d275cbd8cffd42eb21e89d6f0721af328bb9
willyanssilva <willyansdare50@gmail.com>
2025-01-04 17:40:08 -0300

    Foi acrecentado o if

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const botton = document.querySelector('.botao-principal')
+const seletorDemoeda = document.querySelector('.seletor-de-moeda')
+
 function conversor() {
     const valor = document.querySelector('.valor-digitado').value
     const valorAconverter = document.querySelector('.moeda-para-converter')
     const valorConvertido = document.querySelector('.moeda-convertida')
+
+    console.log(seletorDemoeda.value)
+
     const dolar = 6.15
-    const conversao = valor / dolar
+    const euro = 6.37
+     
+
+    if(seletorDemoeda.value == 'dolar'){
+        valorConvertido.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
+            valor / dolar,
+        )
+    }
+
+    if(seletorDemoeda.value == 'euro'){
+        valorConvertido.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
+            valor / euro,
+        )
+    }
 
     valorAconverter.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
         valor,
     )
-    valorConvertido.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(
-        conversao,
-    )
+    
 
 }
 
